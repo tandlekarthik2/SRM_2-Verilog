129721d582a44b93daf8fff2addcb9c5b75636a4
Sudhanyap <sudhanyp@srmist.edu.in>
2025-09-07 20:00:29 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -16,7 +16,7 @@ module tb ();
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
-  reg ena;
+ //reg ena;
   reg [7:0] ui_in;
   reg [7:0] uio_in;
   wire [7:0] uo_out;
@@ -41,7 +41,7 @@ module tb ();
       .uio_in (uio_in),   // IOs: Input path
       .uio_out(uio_out),  // IOs: Output path
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
+      //.ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
